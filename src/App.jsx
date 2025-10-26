984bee7c30082c8f8adf83f70b906bfe17eccf09
Hassan Abdellah <hassanabdellah25@gmail.com>
2025-10-26 21:51:35 +0300

    implemnet showcase section with installtion of GSAP and react-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import ProductViewer from "./Components/ProductViewer";
+import Showcase from "./Components/Showcase";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
+      <Showcase />
     </main>
   );
 }
